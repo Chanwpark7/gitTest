2c7cca81cd33ab00bf60d86e59df20bda6245a96
TJ <TJ@DESKTOP-8FSFFJK>
2024-09-12 16:59:13 +0900

    reset

---
+++ src/eGitTest/Babo.java
@@ -0,0 +1,10 @@
+package eGitTest;
+
+public class Babo {
+	//레포 생성후엔 반드시 접속 권한과 이에 따른 암호 토큰을 발행해야함
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	String ba="ba";
+}
